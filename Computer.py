f5abbdfe1c98fcf1f0e46f6b99550ff89e272942
Emil <emil.karlstrom@gmail.com>
2019-09-25 17:11:43 +0200

    Made computer it programmable

--- Computer.py
+++ Computer.py
@@ -1,6 +1,6 @@
 import re
 import threading
-
+import time
 class INSTRUCTION:
     def __init__(self, NAME, REQUIRES_VALUE, INSTRUCTION_CODE):
         self.NAME = NAME
@@ -26,11 +26,16 @@ class Program:
        self.COMPILED = []
        self.INSTRUCTIONS = [
            INSTRUCTION('NOP', False, 0),
-           INSTRUCTION('MOV', True,  1),
+           INSTRUCTION('MOV', True, 1),
            INSTRUCTION('ADD', True,  2),
            INSTRUCTION('SUB', True,  3),
            INSTRUCTION('OUT', False, 4),
-           INSTRUCTION('HLT', False, 5)
+           INSTRUCTION('HLT', False, 5),
+           INSTRUCTION('JMP', True, 6),
+           INSTRUCTION('JPC', True, 7),
+           INSTRUCTION('JPZ', True, 8),
+           INSTRUCTION('STR', True, 9),
+           INSTRUCTION('GET', True, 10),
        ]
 
     def __call__(self, ASSEMBLY_CODE):
@@ -73,9 +78,9 @@ class Program:
 
 class PYComputer:
     def __init__(self):
-        self.REGISTER = []
+        self.REGISTER_SIZE = 0x10 # Register fits 16 instructions and values
+        self.REGISTER = [0 for i in range(self.REGISTER_SIZE)]
         self.REGISTER_A = 0xff
-        self.REGISTER_SIZE = 0xf # Register fits 16 instructions
         self.PROGRAM_COUNTER = 0
         self.FLAGS = {
             'Z': False, # Flag for value of register A is 0
@@ -87,15 +92,22 @@ class PYComputer:
             self.__ADD,
             self.__SUB,
             self.__OUT,
-            self.__HLT
+            self.__HLT,
+            self.__JMP,
+            self.__JPC,
+            self.__JPZ,
+            self.__STR,
+            self.__GET
         ]
     def run_program(self, program: Program):
         if not program.COMPILE():
             print('EXECUTION ERROR: FAILED TO COMPILE PROGRAM, EXITING')
             return
+        i = 0
         for code in program.COMPILED:
-            if not self.__ADD_TO_REG(code):
+            if not self.__ADD_TO_REG(code, i):
                 return
+            i += 1
         print(f'Program loaded successfully')
         thread = threading.Thread(target=self.__EXECUTE(), args=())
         thread.start()
@@ -105,32 +117,32 @@ class PYComputer:
         self.PROGRAM_COUNTER = 0
         while self.PROGRAM_COUNTER < len(self.REGISTER):
             INSTRUCTION = self.REGISTER[self.PROGRAM_COUNTER]
-            idx, val = INSTRUCTION.INSTRUCTION_CODE, INSTRUCTION.VALUE
-            self.OPERATIONS[idx](val)
+            if isinstance(INSTRUCTION, COMPILED_INSTRUCTION):
+                idx, val = INSTRUCTION.INSTRUCTION_CODE, INSTRUCTION.VALUE
+                self.OPERATIONS[idx](val)
             self.PROGRAM_COUNTER += 1
+            time.sleep(0.1)
         
-    def __ADD_TO_REG(self, ADD):
-        if len(self.REGISTER) >= self.REGISTER_SIZE:
+    def __ADD_TO_REG(self, ADD, POSITION):
+        if POSITION >= self.REGISTER_SIZE:
             print(f'EXECUTION ERROR: ERROR REGISTER OUT OF MEMORY')
             return False
-        self.REGISTER.append(ADD)
+        self.REGISTER[POSITION] = ADD
         return True
-        
-    def __ASYNC_RUN(self):
-        pass
 
     def __NOP(self, *args):
         pass
 
     def __MOV(self, *args):
         self.REGISTER_A = args[0]
-    # Adds VALUE to register A
+        
+    # Adds register at address to register A
     def __ADD(self, *args):
-        self.REGISTER_A = self.REGISTER_A + args[0]
+        self.REGISTER_A = self.REGISTER_A + self.REGISTER[args[0]]
         self.__CHECK_FLAGS()
 
     def __SUB(self, *args):
-        self.REGISTER_A = self.REGISTER_A - args[0]
+        self.REGISTER_A = self.REGISTER_A - self.REGISTER[args[0]]
         self.__CHECK_FLAGS()
 
     # Prints Register A's value
@@ -142,6 +154,28 @@ class PYComputer:
     def __HLT(self, *args):
         self.PROGRAM_COUNTER = len(self.REGISTER)
         
+    def __JMP(self, *args):
+        self.PROGRAM_COUNTER = args[0]
+
+    # Jump if carry
+    def __JPC(self, *args):
+        if self.FLAGS['C']:
+            self.PROGRAM_COUNTER = args[0]
+            print('JPC')
+
+    # Jump if Zero
+    def __JPZ(self, *args):
+        if self.FLAGS['Z']:
+            self.PROGRAM_COUNTER = args[0]
+
+    # Store Register A at register address
+    def __STR(self, *args):
+        self.REGISTER[args[0]] = self.REGISTER_A
+
+    def __GET(self, *args):
+        self.REGISTER_A = self.REGISTER[args[0]]
+        self.__CHECK_FLAGS()
+
     def __CHECK_FLAGS(self):
         self.FLAGS['Z'] = self.REGISTER_A == 0
         if self.REGISTER_A > 0xff or self.REGISTER_A < 0:
